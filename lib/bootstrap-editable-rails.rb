bb1b6ab99a63e42320bf716f8fa051b3cec067d9
Sikandar Tariq <sikandar.tariq@thedevden.co>
2019-10-31 10:59:43 +0500

    Explicitly mention assets to precompile

--- lib/bootstrap-editable-rails.rb
+++ lib/bootstrap-editable-rails.rb
@@ -9,7 +9,7 @@ module Bootstrap
           ::ActiveSupport.on_load(:action_view) do
             ::ActionView::Base.send :include, Bootstrap::Editable::Rails::ViewHelper
           end
-          app.config.assets.precompile << %r(bootstrap-editable/.*\.(?:png|gif)$)
+          app.config.assets.precompile << %w(bootstrap-editable/clear.png bootstrap-editable/loading.gif )
         end
       end
     end
